920d147eeec73aa99e91f5d4766ea9b709e89124
Alexandra Holden <alexandraholden@Alexandras-MacBook-Air.local>
2022-03-15 17:09:30 -0400

    created post get/post/delete routes

--- models/index.js
+++ models/index.js
@@ -1,4 +1,16 @@
 const User = require('./User');
 const Post = require('./Post');
 
+// create User and Post associations
+
+// the User can have many posts
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+// the Post belongs to one User
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
 module.exports = { User, Post };
